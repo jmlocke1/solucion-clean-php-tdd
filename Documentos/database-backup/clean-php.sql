65b332afd4b219af8a942fed746411ed1969eeaf
Josemi Xeon <worldcode.info@gmail.com>
2024-01-11 21:30:19 +0100

    Creada la base de datos y guardado el volcado en el fichero clean-php.sql. Readme.md- Añadidas instrucciones de despliegue de la aplicación

---
+++ Documentos/database-backup/clean-php.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 11-01-2024 a las 20:23:56
+-- Versión del servidor: 8.2.0
+-- Versión de PHP: 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `clean-php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # solucion-clean-php-tdd
 Soolución a la tarea de desarrollo con código limpio y TDD
+
+INSTRUCCIONES DE DESPLIEGUE
+
+1. Crear la base de datos clean-php. El volcado de la base de datos está en Documentos/database-backup/clean-php.sql
+2. Debe crearse el fichero src/config/ConfigLocal.php. Para ello cópiese el fichero ConfigLocalDemo.php y se cambian los valores por los apropiados a tu sistema.
+3. Inicializar librerías de desarrollo con el comando:
+	composer update
